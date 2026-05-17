2fda191ef52991368023a06e78820bcd4f1bf92a
Minh Hiền <hiennguyen080138@gmail.com>
2026-05-18 01:50:34 +0700

    Chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,20 +7,30 @@ import connectDB from './config/db.js';
 import cors from 'cors';
 
 import dotenv from 'dotenv';
+import path from 'path';
+
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve();
 const app = express();
 
 // CORS middleware
-app.use(cors({
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
     origin: 'http://localhost:5173', // Thay đổi nếu frontend chạy trên cổng khác
    
-}));
+    }));
+}
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+}
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log('Server is running on port ' + PORT);
